d8e6f9d5c62f5c3a583a05d6107a350dba9bdfc0
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-11 15:06:15 +0700

    Add login and protected dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+});
+
+Route::middleware('hardcoded.auth')->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
