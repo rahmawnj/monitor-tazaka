d269a05ea2e08f7e1c8984260394279ed1a84555
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-11 15:06:42 +0700

    Use custom session authentication without Laravel guest guard

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-    Route::post('/login', [AuthController::class, 'login'])->name('login.store');
-});
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.store');
 
 Route::middleware('hardcoded.auth')->group(function () {
     Route::get('/', function () {
