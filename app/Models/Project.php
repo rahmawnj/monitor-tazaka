f61b0174c5d6dab78d89fed7821e61d72fde676b
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-14 16:14:33 +0700

    Add project images relationship

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -21,5 +22,10 @@ class Project extends Model
             'latlong' => 'array',
             'sort_order' => 'integer',
         ];
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProjectImage::class)->orderBy('sort_order');
     }
 }
