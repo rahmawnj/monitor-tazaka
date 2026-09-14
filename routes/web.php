b362bbe05a42234f39b609b1578b7cd762a25c29
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-14 15:22:16 +0700

    Add project reorder route

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,13 @@ use App\Http\Controllers\MonitorController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-// Public monitor screen.
 Route::get('/', [MonitorController::class, 'index'])->name('monitor');
 Route::get('/monitor', [MonitorController::class, 'index']);
 Route::get('/monitor/data', [MonitorController::class, 'data'])->name('monitor.data');
 
-// Project management is currently public while authentication is disabled.
 Route::get('/dashboard', fn () => redirect()->route('admin.projects.index'))->name('dashboard');
 Route::get('/admin/projects', [ProjectController::class, 'index'])->name('admin.projects.index');
 Route::post('/admin/projects', [ProjectController::class, 'store'])->name('admin.projects.store');
 Route::put('/admin/projects/{project}', [ProjectController::class, 'update'])->name('admin.projects.update');
+Route::post('/admin/projects/reorder', [ProjectController::class, 'reorder'])->name('admin.projects.reorder');
 Route::delete('/admin/projects/{project}', [ProjectController::class, 'destroy'])->name('admin.projects.destroy');
