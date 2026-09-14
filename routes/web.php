6d7c664af49400568a6c6f89074caedd2ac11dc5
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-14 15:31:42 +0700

    Separate project create, detail, and edit pages

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,10 @@ Route::get('/monitor/data', [MonitorController::class, 'data'])->name('monitor.d
 
 Route::get('/dashboard', fn () => redirect()->route('admin.projects.index'))->name('dashboard');
 Route::get('/admin/projects', [ProjectController::class, 'index'])->name('admin.projects.index');
+Route::get('/admin/projects/create', [ProjectController::class, 'create'])->name('admin.projects.create');
 Route::post('/admin/projects', [ProjectController::class, 'store'])->name('admin.projects.store');
+Route::get('/admin/projects/{project}', [ProjectController::class, 'show'])->name('admin.projects.show');
+Route::get('/admin/projects/{project}/edit', [ProjectController::class, 'edit'])->name('admin.projects.edit');
 Route::put('/admin/projects/{project}', [ProjectController::class, 'update'])->name('admin.projects.update');
 Route::patch('/admin/projects/{project}/progress', [ProjectController::class, 'updateProgress'])->name('admin.projects.progress');
 Route::post('/admin/projects/reorder', [ProjectController::class, 'reorder'])->name('admin.projects.reorder');
