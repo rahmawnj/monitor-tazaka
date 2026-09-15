eabbaf72088d93b5cba9faf5c383533693931b44
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-15 13:40:01 +0700

    Guard duplicate Reverb project updates

--- resources/views/monitor.blade.php
+++ resources/views/monitor.blade.php
@@ -1,4 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/pusher-js@8.4.0/dist/web/pusher.min.js"></script>
+@include('partials.reverb-event-guard')
 @include('partials.monitor-page')
 @include('partials.last-updated')
 @include('partials.detail-realtime-sync')
