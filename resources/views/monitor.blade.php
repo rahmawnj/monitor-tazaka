938e511e090db9aad80250f2d78a2bf783c11b1f
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-15 12:27:31 +0700

    Remove unnecessary media blocks from project detail modal

--- resources/views/monitor.blade.php
+++ resources/views/monitor.blade.php
@@ -3,7 +3,6 @@
 @include('partials.last-updated')
 @include('partials.detail-realtime-sync')
 @include('partials.detail-image-realtime-fix')
-@include('partials.detail-media-fix')
 @include('partials.map-fallback')
 @include('partials.company-brand-fix')
 @include('partials.map-height-fix')
